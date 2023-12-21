aa6f2267a5e32ae7a56ce7a51d6276c143133349
Mafas325 <maty.fulin@gmail.com>
2023-12-21 16:56:57 +0100

    trojka done

--- tasks/1.3 - merge/main.js
+++ tasks/1.3 - merge/main.js
@@ -14,17 +14,31 @@ function setup() {
 
 
 
-
 function merge(listA, listB) {
     let out = [];
-    let délka = listA.lenght + listB.lenght
+    let délka = listA.length + listB.length
     let a = 0
-    while (délka < a){
+    let b = 0
+    for(let i = 0; i < délka; i++){
+        if (a >= listA.length){
+            out.push(listB[b])
+            b = b+1
+        }
+        else if (b >= listB.length){
+            out.push(listA[a])
+            a = a+1
+        }
         
+        else if (listA[a] < listB[b]){
+            out.push(listA[a])
+            a=a+1
+        }
+        else {
+            out.push(listB[b])
+            b=b+1
+        }
 
-
-
-        a = a + 1
+        
     }
     
 
