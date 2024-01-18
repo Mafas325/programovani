46b2f0ce687b3486219285220667ab331d188466
Mafas325 <maty.fulin@gmail.com>
2024-01-18 15:40:14 +0100

    1.4

--- tasks/1.4 - mergesort/main.js
+++ tasks/1.4 - mergesort/main.js
@@ -16,6 +16,53 @@ function setup() {
 // Tomu se říká rekurze
 // Je to často velmi jednoduchý způsob jak naprogramovat něco složitého, ale pozor na to aby opakované volání funkce někdy skončilo
 // Je velmi lehké pomocí rekurze vytvořit program, který nikdy neskončí
+
+
+
+
+
+
+function merge(listA, listB) {
+    let out = [];
+    let délka = listA.length + listB.length
+    let a = 0
+    let b = 0
+    for(let i = 0; i < délka; i++){
+        if (a >= listA.length){
+            out.push(listB[b])
+            b = b+1
+        }
+        else if (b >= listB.length){
+            out.push(listA[a])
+            a = a+1
+        }
+        
+        else if (listA[a] < listB[b]){
+            out.push(listA[a])
+            a=a+1
+        }
+        else {
+            out.push(listB[b])
+            b=b+1
+        }
+
+        
+    }
+    
+
+    return out;
+}
+
+
+
 function mergesort(list) {
-    return list;
+    if (list.length <= 1) return list
+
+    let right=list.slice(list.length/2, list.length)
+    let left=list.slice(0, list.length/2)
+    
+    left = mergesort(left)
+    right = mergesort(right)
+
+    return merge(left, right);
 }
