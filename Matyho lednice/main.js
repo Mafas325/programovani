78bfb37b80744e0026d9ffea4f36c2f6fa91067c
Mafas325 <maty.fulin@gmail.com>
2024-01-25 17:05:13 +0100

    gravitace

--- Matyho lednice/main.js
+++ Matyho lednice/main.js
@@ -1,14 +1,31 @@
 let seznam = []
-
+let gravity = 0.5
 
 
 class Circle{
     constructor(x, y){
         this.směr_b = createVector(x, y)
+        this.vel = createVector();
+        
     }
     draw(){
         circle(this.směr_b.x, this.směr_b.y, 20)
     } 
+    update() {
+
+        this.vel.add(0, gravity)
+
+        
+        if (this.směr_b.y>height - 10){
+            this.vel.y = this.vel.y*-0.95
+            //this.směr_b.y = height - 10
+        }
+        this.směr_b.add(this.vel);
+    }
+    
+
+
+
 
 
 }
@@ -16,7 +33,7 @@ class Circle{
 
 
 function setup(){
-createCanvas(400, 400)
+createCanvas(800, 600)
 background(51)
 for(let i =0; i<100; i++){
     seznam.push(new Circle(random(width), random(height)))
@@ -25,6 +42,7 @@ for(let i =0; i<100; i++){
 
 }
 function draw (){
-
+    background(0)
+    for(let circle of seznam) circle.update()
     for(let circle of seznam) circle.draw()
 }
