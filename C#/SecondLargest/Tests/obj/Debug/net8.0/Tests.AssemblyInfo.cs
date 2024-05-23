67698b71e8c27e7de5e71931e93f35993e91a0d0
Mafas325 <maty.fulin@gmail.com>
2024-05-23 16:52:25 +0200

    pocasi hotove

--- C#/SecondLargest/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
+++ C#/SecondLargest/Tests/obj/Debug/net8.0/Tests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+602213b2edb2b158b748d6a2ed0ae23490d7912c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+94a905a31d3eb0ebdbf50d11d2415ad0461d64fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- C#/ZmenyPocasi/ZmenyPocasi/Program.cs
+++ C#/ZmenyPocasi/ZmenyPocasi/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace ZmenyPocasi
+﻿using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace ZmenyPocasi
 {
     public class Program
     {
@@ -9,7 +12,32 @@
 
         public static string VyresProblem(int[] data)
         {
+            int vysledek_potencionalni = 0;
             int vysledek = 0;
+            bool poprve = true;
+            int prvni_cislo = 0;
+            for (int i = 0; i < data.Length; i++)
+            {
+                
+                if (poprve)
+                {
+                    prvni_cislo = data[i];
+                    poprve = false;
+                }
+
+                else
+                {
+                    if (prvni_cislo > data[i])
+                    {
+                    vysledek_potencionalni = prvni_cislo-data[i];
+                    }
+                    else vysledek_potencionalni = data[i] - prvni_cislo;
+
+                    prvni_cislo = data[i];
+                    if (vysledek_potencionalni>vysledek)vysledek = vysledek_potencionalni;
+                }
+            }
+
 
             return vysledek.ToString();
         }
